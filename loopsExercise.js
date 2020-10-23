1f68e8d101f08639387ac0fa3ce51acdae862f0b
Sergio Montano <sergiomontano@BCs-MacBook-Pro-2.local>
2020-10-23 14:20:36 -0500

    harry potter

---
+++ loopsExercise.js
@@ -0,0 +1,27 @@
+for (i = 1; i < 8; i++){
+    console.log(i);
+  };
+
+  for (i = 6; i <= 27; i += 2){
+    console.log(i);
+  }
+
+  const wizards = [
+      "Harry Potter",
+      "Hermione Granger",
+      "Ron Weasley"
+     ];
+    
+
+  for (i = 0; i < wizards.length; i++){
+    console.log(`${i + 1}. ${wizards[i]}`);
+  }
+
+let harryPotterMovies = 0
+while (harryPotterMovies <= 8){
+  console.log(harryPotterMovies);
+  harryPotterMovies++;
+}
+
+
+     
